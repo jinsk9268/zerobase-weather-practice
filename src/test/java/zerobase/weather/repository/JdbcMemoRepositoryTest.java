0b6f1b39c2fdea2fe2b78db3eda8a149e5f1c188
jinsk9268 <jinsk9268@gmail.com>
2023-03-15 14:08:43 +0900

    test: JdbcMemoRepository 유닛 테스트 추가

---
+++ src/test/java/zerobase/weather/repository/JdbcMemoRepositoryTest.java
@@ -0,0 +1,43 @@
+package zerobase.weather.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional // db에 저장하지 않음
+class JdbcMemoRepositoryTest {
+    @Autowired
+    JdbcMemoRepository jdbcMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        // given
+        Memo newMemo = new Memo(2, "insert memo test");
+
+        // when
+        jdbcMemoRepository.save(newMemo);
+
+        // then
+        Optional<Memo> result = jdbcMemoRepository.findById(2);
+        assertEquals(result.get().getText(), "insert memo test");
+    }
+
+    @Test
+    void findAllMemoTest() {
+        // given
+        List<Memo> memoList = jdbcMemoRepository.findAll();
+
+        // when
+        // then
+        System.out.println(memoList);
+        assertNotNull(memoList);
+    }
+}
