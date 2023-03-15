882eaeea2d977a6bf12671b341b19809c7f507e5
jinsk9268 <jinsk9268@gmail.com>
2023-03-15 14:53:59 +0900

    feat: Memo 테이블 클래스에 jpa 활용을 위한 Entity 설정 추가

--- src/main/java/zerobase/weather/domain/Memo.java
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -5,11 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "memo")
 public class Memo {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 키생성 데이터베이스에 맡기겠다
     private int id;
     private String text;
 }
