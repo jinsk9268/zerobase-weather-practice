42006fd84873d80d6a2ee2ec77bb8f9dfcdd231d
jinsk9268 <jinsk9268@gmail.com>
2023-03-16 17:15:34 +0900

    feat: diary db에 삽입 추가

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,10 +1,13 @@
 package zerobase.weather.service;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import zerobase.weather.domain.Diary;
+import zerobase.weather.repository.DiaryRepository;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -18,6 +21,11 @@ import java.util.Map;
 public class DiaryService {
     @Value("${openweathermap.key}")
     private String apiKey;
+    private final DiaryRepository diaryRepository;
+
+    public DiaryService(DiaryRepository diaryRepository) {
+        this.diaryRepository = diaryRepository;
+    }
 
     public void createDiary(LocalDate date, String text) {
         // openweathermap에서 날씨 데이터 가져오기
@@ -25,6 +33,15 @@ public class DiaryService {
 
         // 받아온 날씨 json 파싱하기
         Map<String, Object> parsedWeather = parseWeather(weatherData);
+
+        // 파싱된 데이터 + 일기 값 db에 insert
+        Diary nowDiary = new Diary();
+        nowDiary.setWeather(parsedWeather.get("main").toString());
+        nowDiary.setIcon(parsedWeather.get("icon").toString());
+        nowDiary.setTemperature((Double)parsedWeather.get("temp"));
+        nowDiary.setText(text);
+        nowDiary.setDate(date);
+        diaryRepository.save(nowDiary);
     }
 
     private String getWeatherString() {
@@ -67,7 +84,7 @@ public class DiaryService {
 
     private Map<String, Object> parseWeather(String jsonString) {
         JSONParser jsonParser = new JSONParser();
-        JSONObject jsonObject = new JSONObject();
+        JSONObject jsonObject;
 
         try {
             jsonObject = (JSONObject) jsonParser.parse(jsonString);
@@ -80,7 +97,7 @@ public class DiaryService {
         JSONObject mainData = (JSONObject) jsonObject.get("main");
         resultMap.put("temp", mainData.get("temp"));
 
-        JSONObject weatherData = (JSONObject) jsonObject.get("weather");
+        JSONObject weatherData = (JSONObject) ((JSONArray) jsonObject.get("weather")).get(0);
         resultMap.put("main", weatherData.get("main"));
         resultMap.put("icon", weatherData.get("icon"));
 
