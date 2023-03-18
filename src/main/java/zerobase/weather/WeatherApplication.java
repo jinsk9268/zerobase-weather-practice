ab9500b2d957880750dee4f9cd1f220222c8b75e
jinsk9268 <jinsk9268@gmail.com>
2023-03-18 21:52:40 +0900

    feat: 프로젝트에서 scheduling 처리 가능하도록 추가

--- src/main/java/zerobase/weather/WeatherApplication.java
+++ src/main/java/zerobase/weather/WeatherApplication.java
@@ -2,10 +2,12 @@ package zerobase.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement // 프로젝트 안에서 transaction 동작 가능
+@EnableScheduling // 프로젝트 안에서 scheduling 동작 가능
 public class WeatherApplication {
 
 	public static void main(String[] args) {
