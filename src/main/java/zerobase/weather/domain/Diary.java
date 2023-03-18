bf6b76dd5901f387299ea0b8b8bd58819b9d2d10
jinsk9268 <jinsk9268@gmail.com>
2023-03-18 22:21:20 +0900

    feat: Diary 객체 날씨 데이터 저장하는 메서드 추가
    - setDateWeather

--- src/main/java/zerobase/weather/domain/Diary.java
+++ src/main/java/zerobase/weather/domain/Diary.java
@@ -23,4 +23,11 @@ public class Diary {
     private double temperature;
     private String text;
     private LocalDate date;
+
+    public void setDateWeather(DateWeather dateWeather) {
+        this.weather = dateWeather.getWeather();
+        this.icon = dateWeather.getIcon();
+        this.temperature = dateWeather.getTemperature();
+        this.date = dateWeather.getDate();
+    }
 }
