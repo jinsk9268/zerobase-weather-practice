5d42eae68e19b035a2284819fa7e54e93e11b55a
jinsk9268 <jinsk9268@gmail.com>
2023-03-18 20:34:25 +0900

    feat: 프로젝트에서 transaction 처리 가능하도록 추가

--- src/main/java/zerobase/weather/WeatherApplication.java
+++ src/main/java/zerobase/weather/WeatherApplication.java
@@ -2,8 +2,10 @@ package zerobase.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement // 프로젝트 안에서 transaction 동작 가능
 public class WeatherApplication {
 
 	public static void main(String[] args) {
