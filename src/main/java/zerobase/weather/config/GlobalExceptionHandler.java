46c7b48bf41ba4f9f8e9ed0a5146b46ba87f952e
jinsk9268 <jinsk9268@gmail.com>
2023-03-19 19:58:54 +0900

    feat: 전역 에러 핸들러 추가
    - GlobalExceptionHandler

---
+++ src/main/java/zerobase/weather/config/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package zerobase.weather.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import zerobase.weather.WeatherApplication;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(WeatherApplication.class);
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public Exception handleAllException() {
+       logger.error("error from GlobalExceptionHandler");
+       return new Exception();
+    }
+}
